d47c25e2fd58e80c7f2bc9e3c2b63edacfc00673
bladimirw <114620648+bladimirw@users.noreply.github.com>
2024-10-06 22:28:47 -0600

    Creacion de mi Primer Componente Reactjs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-
-
-
 const rootElement = document.getElementById("root");
 
-ReactDOM.createRoot(rootElement).render(<h1>Aprendiendo Reactjs Bladimir</h1>);
+function DivBestialAhorro() {
+  return <div>
+    <h1>Bestial Ahorro</h1>
+    <p>¡Ahorra en grande!</p>
+  </div>;
+}
+
+function TarjetaDeCredito() {
+  return <div>
+    <h1>Tarjeta de crédito</h1>
+    <p>¡Paga en grande!</p>
+  </div>;
+}
+
+
+ReactDOM.createRoot(rootElement).render(
+  <div>
+    <DivBestialAhorro />
+    <DivBestialAhorro />
+    <TarjetaDeCredito />
+    <TarjetaDeCredito />
+  </div>
+);
+
